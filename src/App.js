11eb640b6eba487be5fe781c879b76fe2e0acd25
giovannimedici <giovannigilsu@gmail.com>
2024-06-26 21:23:34 -0300

    connect with api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import { useState } from 'react';
-import './styles.css';
+import { useState, useEffect, setState } from "react";
+import "./styles.css";
 import { FiPlus } from "react-icons/fi";
-import { FiMinus } from 'react-icons/fi';
-import { FiEdit } from 'react-icons/fi';
+import { FiMinus } from "react-icons/fi";
+import { FiEdit } from "react-icons/fi";
+import api from "./api";
 
 function App() {
-  const [task, setTask] = useState('');
+  const [task, setTask] = useState("");
   const [taskList, settaskList] = useState([]);
   const [editedTask, seteditedTask] = useState(null);
 
+  useEffect(() => {
+    api
+      .get()
+      .then((response) => settaskList(response.data))
+      .catch((err) => {
+        console.error(err.message);
+      });
+  });
 
   function add() {
-    if (task === '') {
-      alert('Preencher a tarefa');
+    if (task === "") {
+      alert("Preencher a tarefa");
       return;
     }
-    settaskList([...taskList, { id: Date.now().toString(), title: task }])
-    setTask('');
+    const obj = {
+      title: task,
+    };
+
+    try {
+      api.post("http://localhost:3333/tasks", obj).then((response) => {
+        console.log(response.status, response.data);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    
+    setTask("");
   }
 
   const remove = (id) => {
-    const newList = taskList.filter((item) => item.id !== id)
-    settaskList(newList);
-  }
+    api.delete(`http://localhost:3333/tasks/${id}`);
+  };
 
   function edit(item) {
     seteditedTask(item);
@@ -30,33 +49,33 @@ function App() {
   }
 
   function update() {
-    const updatedTaskList = taskList.map((item) => {
-      if (item.id === editedTask.id) {
-        return { ...item, title: task }
-      }
+    const obj = {
+      title: task,
+    };
 
-      return item;
-    });
+    api
+      .put(`http://localhost:3333/tasks/${editedTask.id}`, obj)
 
-    settaskList(updatedTaskList);
-    seteditedTask(null);
     setTask("");
+    seteditedTask(null)
   }
 
   const listItems = taskList.map((t) => {
-    return <li key={t.id}>
-      <div className='displayContainer'>
-        {t.title}
-        <div className='displayButtons'>
-          <button className='buttonEdit' onClick={() => remove(t.id)}>
-            <FiMinus size={20} color="#FFF" />
-          </button>
-          <button className='buttonEdit' onClick={() => edit(t)}>
-            <FiEdit size={20} color="#FFF" />
-          </button>
+    return (
+      <li key={t.id}>
+        <div className="displayContainer">
+          {t.title}
+          <div className="displayButtons">
+            <button className="buttonEdit" onClick={() => remove(t.id)}>
+              <FiMinus size={20} color="#FFF" />
+            </button>
+            <button className="buttonEdit" onClick={() => edit(t)}>
+              <FiEdit size={20} color="#FFF" />
+            </button>
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+    );
   });
 
   return (
@@ -69,19 +88,19 @@ function App() {
           value={task}
           onChange={(e) => setTask(e.target.value)}
         />
-        {!editedTask ? <button className='buttonAdd' onClick={add}>
-          <FiPlus size={25} color="#FFF" />
-        </button> :
-          <button className='buttonAdd' onClick={() => update()}>
+        {!editedTask ? (
+          <button className="buttonAdd" onClick={add}>
+            <FiPlus size={25} color="#FFF" />
+          </button>
+        ) : (
+          <button className="buttonAdd" onClick={update}>
             <FiEdit size={25} color="#FFF" />
-          </button>}
+          </button>
+        )}
       </div>
 
-      {taskList.length > 0 &&
-        <div className='divList'>{listItems}</div>
-      }
+      {taskList.length > 0 && <div className="divList">{listItems}</div>}
     </div>
-
   );
 }
 
